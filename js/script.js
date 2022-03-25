774e2b8ce578dba574920644c441fdee9f3c1101
Nicolas Marri <nicomarri22@gmail.com>
2022-03-25 09:52:54 -0300

    Implements onscroll on menu

--- js/script.js
+++ js/script.js
@@ -5,3 +5,25 @@ menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+window.onscroll = () =>{
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active')
+    }else{
+        document.querySelector('.header').classList.remove('active')
+    }
+
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+window.onload = () =>{
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active')
+    }else{
+        document.querySelector('.header').classList.remove('active')
+    }
+}
